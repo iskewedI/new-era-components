c270ebb0cc81846d61040ceac65aaa811b32b5ec
iskewedi <jtornello2019@hotmail.com>
2022-10-04 11:19:20 -0300

    - Created Input UI component. - Created EditableText UX Component. It's default rendered as a normal text, but double clicking it will be able to edit it.

---
+++ src/components/UI/Input/index.tsx
@@ -0,0 +1,66 @@
+import { useState } from 'react';
+import styles from './styles.module.css';
+
+export interface InputProps {
+  onChange?: (value: string) => void;
+  placeholder?: string;
+  onSubmit?: (text: string) => void;
+  autofocus?: boolean;
+  inputClasses?: string;
+  startValue?: string;
+  formStyle?: React.CSSProperties;
+}
+
+/**
+ * Renders a Form with Input component, to handle the onChange and onSubmit event properly.
+ * @param {string} placeholder - Text for the input placeholder.
+ * @param {(text: string) => void} onSubmit - Callback function to be called in the onSubmit event.
+ * @param {(value: string) => void} onChange - Callback function to be called in the onChange event.
+ * @param {boolean} [autofocus=false] - Defines if the input should be autofocused when mounted in the UI.
+ * @param {string} startValue - Default value of the input.
+ * @param {CSSProperties} formStyle - Object with Javascript styles to be applied to the form.
+ */
+const Input = ({
+  placeholder = '',
+  onSubmit,
+  onChange,
+  autofocus = false,
+  startValue = '',
+  formStyle,
+}: InputProps) => {
+  const [value, setValue] = useState<string>(startValue);
+
+  const handleChange = (value: string) => {
+    setValue(value);
+
+    if (onChange && typeof onChange === 'function') {
+      onChange(value);
+    }
+  };
+
+  /***
+   * The evt.preventDefault() is used to prevent the reloading of the page on the submit.
+   * The submit event is used to handle the user input submit with the keyboard and different devices (compatibility).
+   */
+  const handleSubmit = (evt: React.ChangeEvent<HTMLFormElement>) => {
+    evt.preventDefault();
+
+    if (onSubmit && typeof onSubmit === 'function') {
+      onSubmit(value);
+    }
+  };
+
+  return (
+    <form onSubmit={handleSubmit} style={formStyle}>
+      <input
+        value={value}
+        onChange={evt => handleChange(evt.target.value)}
+        className={styles.input}
+        placeholder={placeholder}
+        autoFocus={autofocus}
+      />
+    </form>
+  );
+};
+
+export default Input;
---
+++ src/components/UX/EditableText/EditableText.tsx
@@ -0,0 +1,93 @@
+import { useEffect, useState } from 'react';
+import Input from '../../UI/Input';
+
+interface EditableTextProps {
+  editing?: boolean;
+  text?: string;
+  onChange?: (newValue: string) => void;
+  onSubmit?: (newValue: string) => void;
+  onEditStart?: () => void;
+  containerStyles?: string;
+  enabled?: boolean;
+  controlled?: boolean;
+}
+
+const EditableText = ({
+  editing = false,
+  text = '',
+  containerStyles = '',
+  enabled = true,
+  controlled = false,
+  onChange,
+  onSubmit,
+  onEditStart,
+}: EditableTextProps) => {
+  const [isEditing, setIsEditing] = useState(editing);
+  const [textValue, setTextValue] = useState(text);
+
+  const handleChange = (value: string) => {
+    if (onChange && typeof onChange === 'function') {
+      onChange(value);
+    }
+  };
+
+  const handleSubmit = (value: string) => {
+    if (onSubmit && typeof onSubmit === 'function') {
+      onSubmit(value);
+    }
+
+    setTextValue(value);
+    setIsEditing(false);
+  };
+
+  /***
+   * Used in the container.
+   * Handles the double click event.
+   * It'll only work if the component is enabled, and will call the onEditStart callback if is editing.
+   */
+  const handleDoubleClick = () => {
+    if (!enabled) return;
+
+    if (!isEditing === true) {
+      if (onEditStart && typeof onEditStart === 'function') {
+        onEditStart();
+      }
+    }
+
+    setIsEditing(editing => !editing);
+  };
+
+  /***
+   * ONLY used if set as controlled component.
+   * Hook used to ensure this component is updated if the props changes.
+   * It'll force render the component if something has changed.
+   */
+  useEffect(() => {
+    if (controlled) {
+      if (editing !== isEditing) {
+        setIsEditing(editing);
+      }
+
+      if (text !== textValue) {
+        setTextValue(text);
+      }
+    }
+  }, [controlled, editing, isEditing, text, textValue]);
+
+  return (
+    <div className={containerStyles} onDoubleClick={handleDoubleClick}>
+      {!isEditing && textValue}
+      {isEditing && (
+        <Input
+          startValue={textValue}
+          onChange={handleChange}
+          onSubmit={handleSubmit}
+          autofocus={true}
+          formStyle={text ? { width: `${text.length}ch` } : undefined}
+        />
+      )}
+    </div>
+  );
+};
+
+export default EditableText;
